8dc986c50d8495d21af025c3dc7d7f3efa1ead43
Adrian2038 <goglezhu11@gmail.com>
2015-03-30 17:46:08 +0800

    Creating a new graduation-project project
    Using nib than storyboard

---
+++ ZZSetCard/ZZSetCard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZZSetCard
+//
+//  Created by Zhu Dengquan on 15/3/30.
+//  Copyright (c) 2015年 Zhu Dengquan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZZSetCard/ZZSetCard/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ZZSetCard
+//
+//  Created by Zhu Dengquan on 15/3/30.
+//  Copyright (c) 2015年 Zhu Dengquan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
