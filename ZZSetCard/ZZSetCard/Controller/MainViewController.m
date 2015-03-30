c331c62b8ed569a1070a99fb3832d20a251a3cb2
Adrian2038 <goglezhu11@gmail.com>
2015-03-30 18:17:23 +0800

    Add some images and a .pch file

--- ZZSetCard/ZZSetCard/Controller/MainViewController.m
+++ ZZSetCard/ZZSetCard/Controller/MainViewController.m
@@ -17,8 +17,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
-    
-    self.view.backgroundColor = [UIColor purpleColor];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -26,14 +24,5 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
