5165dbe473e1f80abfa626deaf6c381812db89b1
Adrian2038 <goglezhu11@gmail.com>
2015-03-31 18:07:05 +0800

    I will try to complete the project after the Tomb Sweeping Day
    I may copy lots of code from the CardGames , and complete three parts
    of project. BangBangBang !!!

---
+++ BangBangBang/BangBangBang/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BangBangBang
+//
+//  Created by Zhu Dengquan on 15/3/31.
+//  Copyright (c) 2015年 Zhu Dengquan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
