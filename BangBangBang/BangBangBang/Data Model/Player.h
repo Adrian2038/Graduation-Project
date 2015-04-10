0f9015a264bfd10525e89f9d5bf8584c447cc108
Adrian2038 <goglezhu11@gmail.com>
2015-04-10 09:58:03 +0800

    Add a model class : Player
    The Player class is to represent the user

---
+++ BangBangBang/BangBangBang/Data Model/Player.h
@@ -0,0 +1,25 @@
+//
+//  Player.h
+//  BangBangBang
+//
+//  Created by Zhu Dengquan on 15/4/10.
+//  Copyright (c) 2015年 Zhu Dengquan. All rights reserved.
+//
+
+typedef enum
+{
+  PlayerPositionBotton, // the user
+  PlayerPositionLeft,
+  PlayerPositionTop,
+  PlayerPositionRight
+}
+PlayerPosition;
+
+@interface Player : NSObject
+
+@property (nonatomic, assign) PlayerPosition position;
+
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSString *peerID;
+
+@end
---
+++ BangBangBang/BangBangBang/Data Model/Player.m
@@ -0,0 +1,27 @@
+//
+//  Player.m
+//  BangBangBang
+//
+//  Created by Zhu Dengquan on 15/4/10.
+//  Copyright (c) 2015年 Zhu Dengquan. All rights reserved.
+//
+
+#import "Player.h"
+
+@implementation Player
+
+
+- (NSString *)description
+{
+  return [NSString stringWithFormat:@"%@, peerID = %@, name = %@, position = %d",
+          [super description], self.peerID, self.name, self.position];
+}
+
+#pragma mark - Dealloc
+
+- (void)dealloc
+{
+  NSLog(@"dealloc %@", self);
+}
+
+@end
