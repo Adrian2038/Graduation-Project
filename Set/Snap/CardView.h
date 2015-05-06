f1925d482f64b4e69c229f1f2ea72fa0cfe78752
Adrian2038 <goglezhu11@gmail.com>
2015-05-06 22:31:19 +0800

    Add CardView class
    I would  set the CardView a corner radius property, so the image will
    look pretty cool

---
+++ Set/Snap/CardView.h
@@ -0,0 +1,26 @@
+//
+//  CardView.h
+//  Snap
+//
+//  Created by Adrian on 15/5/6.
+//  Copyright (c) 2015年 Hollance. All rights reserved.
+//
+// .........................
+// The default set image is rect , and with the game's effect,
+// I will let the CardView has some corner radius property.
+
+#import <UIKit/UIKit.h>
+
+const CGFloat cardWidth;
+const CGFloat cardHeight;
+
+@class Card;
+// I needn't the animat the card to the palyer's position , but animate the card to a H & V
+
+@interface CardView : UIView
+
+@property (nonatomic, strong) Card *card;
+
+- (void)animationDealingToPosition:(CGPoint)point withDelay:(NSTimeInterval)delay;
+
+@end
---
+++ Set/Snap/CardView.m
@@ -0,0 +1,21 @@
+//
+//  CardView.m
+//  Snap
+//
+//  Created by Adrian on 15/5/6.
+//  Copyright (c) 2015年 Hollance. All rights reserved.
+//
+
+#import "CardView.h"
+
+@implementation CardView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
